9f748607a73f0a9d45306cd8c8d767d0e694e25d
Fábio Mendonça <fabio.jose.mendonca@gmail.com>
2020-03-25 08:39:00 -0300

    aula 44

--- src/app/restaurants/restaurant/restaurant.service.ts
+++ src/app/restaurants/restaurant/restaurant.service.ts
@@ -1,10 +1,13 @@
 import { Restaurant } from "./restaurant.model";
-import { Injectable } from "@angular/core";
+import { Injectable} from "@angular/core";
 import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
 
+import {ErrorHandler} from "app/app.erro-handler"
+
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 @Injectable()
 
@@ -14,7 +17,8 @@ export class RestaurantService {
   constructor(private http: Http) { }
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`)      
+    return this.http.get(`${MEAT_API}/restaurants1`)      
       .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
   }
 }
