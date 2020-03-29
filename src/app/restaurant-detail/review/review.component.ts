dc8410681469afc8c3b3d39f2f280687370f4a12
Fábio Mendonça <fabio.jose.mendonca@gmail.com>
2020-03-29 17:10:09 -0300

    aula 50

--- src/app/restaurant-detail/review/review.component.ts
+++ src/app/restaurant-detail/review/review.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { RestaurantService } from 'app/restaurants/restaurant/restaurant.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-review',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>
+
+  constructor(private restaurantsService: RestaurantService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews = this.restaurantsService.reviewsOfRestaurant(
+      this.route.parent.snapshot.params['id']
+    );
   }
 
 }
--- src/app/restaurants/restaurant/restaurant.service.ts
+++ src/app/restaurants/restaurant/restaurant.service.ts
@@ -1,10 +1,10 @@
 import { Restaurant } from "./restaurant.model";
-import { Injectable} from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
 
-import {ErrorHandler} from "app/app.erro-handler"
+import { ErrorHandler } from "app/app.erro-handler"
 
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
@@ -18,14 +18,20 @@ export class RestaurantService {
   constructor(private http: Http) { }
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`)      
+    return this.http.get(`${MEAT_API}/restaurants`)
       .map(response => response.json())
       .catch(ErrorHandler.handlerError)
   }
 
-  restaurantById(id: string):Observable<Restaurant>{
+  restaurantById(id: string): Observable<Restaurant> {
     return this.http.get(`${MEAT_API}/restaurants/${id}`)
-    .map(response => response.json())
-    .catch(ErrorHandler.handlerError)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError)
+  }
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handlerError);
   }
 }
