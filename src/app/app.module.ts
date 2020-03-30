892c6513988e69bb53575fbfa0df8a85baedc358
Fábio Mendonça <fabio.jose.mendonca@gmail.com>
2020-03-29 22:06:33 -0300

    aula 54

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -43,7 +43,7 @@ import { ShoppingService } from './restaurant-detail/shopping/shopping.service';
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantService, ShoppingService],
+  providers: [RestaurantService, ShoppingService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
