20d26ac0215a5a47c94aca859f48100ec47900e7
Fábio Mendonça <fabio.jose.mendonca@gmail.com>
2020-04-06 23:00:13 -0300

    aula 72

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,6 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -12,6 +13,8 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
+  orderForm: FormGroup;
+
   paymentOptions: RadioOption[] = [
     { 'label': "Dinheiro", value: 'MON' },
     { 'label': "Cartão", value: 'DEB' },
@@ -19,11 +22,21 @@ export class OrderComponent implements OnInit {
   ]
 
   constructor(private orderService: OrderService,
-    private router: Router) { }
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   delivery: number = 8;
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control(''),
+      email: this.formBuilder.control(''),
+      emailConfirmed: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),      
+      paymentOption: this.formBuilder.control(''),
+    })
   }
 
   itemsValue(): number {
