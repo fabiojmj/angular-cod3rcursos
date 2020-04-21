637ee73c2e41d4c1ae3abea4ddeaf402ba2526b2
Fábio Mendonça <fabio.jose.mendonca@gmail.com>
2020-04-21 20:16:04 -0300

    aula 103

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Restaurant } from './restaurant/restaurant.model';
 import { RestaurantService } from './restaurant/restaurant.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'mt-restaurants',
   templateUrl: './restaurants.component.html',
-  styleUrls: ['./restaurants.component.css']
+  styleUrls: ['./restaurants.component.css'],
+  animations: [
+    trigger('toogleSeach', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px"
+      })),
+      state('visible', style({
+        opacity:1,
+        "max-height":"70px",
+        "margin-top":"20px"
+      })),
+      transition('* => *',animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
+
+  searchBarState = 'hidden';
 
   restaurants: Restaurant[];
 
@@ -16,6 +34,10 @@ export class RestaurantsComponent implements OnInit {
   ngOnInit() {
     this.restauranteService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants);
+  }
+
+  toogleSearch(){
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
   }
 
 }
