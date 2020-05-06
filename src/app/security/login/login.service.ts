d4a8d76d4d9b8b45b5291d8c8c4c22457633989b
Fábio Mendonça <fabio.jose.mendonca@gmail.com>
2020-05-06 08:17:09 -0300

    aula 122

--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -4,15 +4,22 @@ import { Observable } from "rxjs/Observable";
 import { MEAT_API } from "app/app.api";
 import { User } from "./user.model";
 import 'rxjs/add/operator/do'
-import { Route, Router } from "@angular/router";
+import 'rxjs/add/operator/filter'
+import { Route, Router, NavigationEnd } from "@angular/router";
 
 @Injectable()
-export class LoginService {   
+export class LoginService {
+
 
     user: User
+    lastUrl: string
 
     constructor(private http: HttpClient,
-        private router: Router) { }
+        private router: Router) {
+        this.router.events
+            .filter(e => e instanceof NavigationEnd)
+            .subscribe((e: NavigationEnd) => this.lastUrl = e.url)
+    }
 
     isLoggedIn(): boolean {
         return this.user !== undefined
@@ -25,7 +32,11 @@ export class LoginService {
         }).do(user => this.user = user)
     }
 
-    handleLogin(path?: string) {
-        this.router.navigate(['/login',btoa(path)])
+    handleLogin(path: string = this.lastUrl) {
+        this.router.navigate(['/login', btoa(path)])
+    }
+
+    logout() {
+        this.user = undefined
     }
 }
